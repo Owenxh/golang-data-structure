5ac35011e0775268819752e00063f01abd0ec925
Owen <sowen1023@gmail.com>
2022-12-05 18:47:23 +0800

    [refactor]

--- util/presum_test.go
+++ util/presum_test.go
@@ -8,12 +8,11 @@ import (
 func TestPreSum(t *testing.T) {
 	nums := []int{-2, 0, 2}
 
-	fmt.Println(buildPreSum(nums))
+	fmt.Println(BuildPreSum(nums))
 
-	fmt.Println(rangeSumCount(nums, 0))
-	fmt.Println(rangeSumCount(nums, -2))
+	fmt.Println(RangeSumCount(nums, 0))
+	fmt.Println(RangeSumCount(nums, -2))
 
-	fmt.Println(rangeSumCountBad(nums, 0))
-	fmt.Println(rangeSumCountBad(nums, -2))
-
+	fmt.Println(RangeSumCountBad(nums, 0))
+	fmt.Println(RangeSumCountBad(nums, -2))
 }
